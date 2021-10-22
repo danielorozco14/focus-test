ca66dc811ff7bd393006bb989a80704ddc1d3112
danielorozco14 <00200617@uca.edu.sv>
2021-10-22 17:13:18 -0600

    Fetch data done and also added spinner animation

--- src/components/ProductsComponent.js
+++ src/components/ProductsComponent.js
@@ -1,32 +1,55 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import React, { Fragment, useEffect, useState } from "react";
+import * as ReactBootstrap from "react-bootstrap";
 
 const Product = () => {
-  const [listProducts, setListProducts] = useState(null);
-  const [loadingProducts, setLoadingProducts] = useState(false);
+  const [listProducts, setListProducts] = useState([]);
+  const [spinner, setSpinner] = useState(false);
 
-  const listOfProduct = async () => {
-    try {
-      const data = await axios
-        .get("https://vending-machine-test.vercel.app/api/products")
-        .then((res) => {
-          console.log(res.data.data);
-          console.log("DATO 0: " + res.data.data[0].thumbnail);
-          setListProducts(res.data.data);
-        });
-    } catch (e) {
-      console.log(e);
-    }
-  };
   useEffect(() => {
-    listOfProduct();
+    axios
+      .get("https://vending-machine-test.vercel.app/api/products")
+      .then((res) => {
+        // console.log(res);
+        setListProducts(res.data.data);
+        
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+      //State value to show spinner animation
+      setSpinner(true); 
   }, []);
-  
-  return(
-      <Fragment>
 
-      </Fragment>
-  )
+  //  console.log(listProducts);
+  return (
+    <div>
+      <h2>Products</h2>
+      <ReactBootstrap.Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </ReactBootstrap.Spinner>
+      <ReactBootstrap.Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Preparation Time</th>
+            <th>Image</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listProducts.map((e) => (
+            <tr key={e.id}>
+              <td>{e.id}</td>
+              <td>{e.name}</td>
+              <td>{e.preparation_time}</td>
+              <td>{e.thumbnail}</td>
+            </tr>
+          ))}
+        </tbody>
+      </ReactBootstrap.Table>
+    </div>
+  );
 };
 
 export default Product;
