1aa819c43e5b481a2425604d6b3f53c3f029b2d4
danielorozco14 <00200617@uca.edu.sv>
2021-10-24 08:12:50 -0600

    Timer added, still missing dispatched orders functionality

--- src/components/OderComponent.js
+++ src/components/OderComponent.js
@@ -2,22 +2,22 @@ import React, { useRef, useState } from "react";
 import * as ReactBootstrap from "react-bootstrap";
 
 const Orders = (props) => {
+  const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0);
+  const countRef = useRef(count);
+  countRef.current = count;
 
-    const countRef = useRef(count);
-    countRef.current = count;
-
-    const getCountTimeout=(e)=>{
-        setTimeout(()=>{
-            alert(e.name + "IT'S READY!")
-        },e.preparation_time * 1000)
-    }
+  const getCountTimeout = (e) => {
+    setTimeout(() => {
+      alert(e.name + " IT'S READY!");
+    }, e.preparation_time * 1000);
+    
+  };
 
   console.log(props);
   let i = 0;
   return props.row.map((e) => (
-    <tr key={i= i + 1}>
+    <tr key={(i = i + 1)}>
       <td>{e.id}</td>
       <td>{e.name}</td>
       <td>{e.preparation_time}</td>
@@ -29,13 +29,11 @@ const Orders = (props) => {
           src={e.thumbnail}
         />
       </td>
-      <td >
-      <ReactBootstrap.Spinner animation="border" role="status">
+      <td>
+        <ReactBootstrap.Spinner animation="border" role="status">
           <span className="visually-hidden">Loading...</span>
         </ReactBootstrap.Spinner>
-        {
-            getCountTimeout(e)
-        }
+        {getCountTimeout(e)}
       </td>
     </tr>
   ));
