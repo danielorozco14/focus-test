8c163f343a8e3c389aadd7a5609b795a4eb7d6f3
danielorozco14 <00200617@uca.edu.sv>
2021-10-24 17:50:05 -0600

    Orders dispatched table refactor as component

--- src/components/DispatchedComponent.js
+++ src/components/DispatchedComponent.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import * as ReactBootstrap from "react-bootstrap";
 const Dispatched = (props) => {
-
   console.log("PROP COMING FROM ORDER COMPONENT TO DISPATCH COMPONENT");
   console.log(props);
-
 
   return (
     <ReactBootstrap.Table
@@ -20,17 +18,40 @@ const Dispatched = (props) => {
           <th>Name</th>
           <th>Preparation Time</th>
           <th>Image</th>
-          <th>Time</th>
         </tr>
       </thead>
       <tbody>
-        
-          <tr>
-            <td>
-                {props.orders[0].name}
-            </td>
-          </tr>
-        
+        {props.orders.length > 0
+          ? props.orders.map((e, i) => (
+              <tr key={(i = i + 1)}>
+                <td>{e[0].id}</td>
+                <td>{e[0].name}</td>
+                <td>{e[0].preparation_time}</td>
+                <td>
+                  {" "}
+                  <img
+                    style={{ width: "3rem", height: "3rem" }}
+                    alt={e[0].name}
+                    src={e[0].thumbnail}
+                  />
+                </td>
+              </tr>
+            ))
+          : props.orders.map((e, i) => (
+              <tr key={(i = i + 1)}>
+                <td>{e.id}</td>
+                <td>{e.name}</td>
+                <td>{e.preparation_time}</td>
+                <td>
+                  {" "}
+                  <img
+                    style={{ width: "3rem", height: "3rem" }}
+                    alt={e.name}
+                    src={e.thumbnail}
+                  />
+                </td>
+              </tr>
+            ))}
       </tbody>
     </ReactBootstrap.Table>
   );
--- src/components/OderComponent.js
+++ src/components/OderComponent.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState, Fragment } from "react";
 import * as ReactBootstrap from "react-bootstrap";
-//import Dispatched from "./DispatchedComponent";
+import Dispatched from "./DispatchedComponent";
 
 const Orders = (props) => {
   const [count, setCount] = useState(0);
@@ -61,55 +61,7 @@ const Orders = (props) => {
 
       <h2>Orders Dispatched</h2>
 
-      <ReactBootstrap.Table
-        striped
-        bordered
-        hover
-        responsive="lg"
-        name="orders-table"
-      >
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Preparation Time</th>
-            <th>Image</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dispatch.length > 0
-            ? dispatch.map((e, i) => (
-                <tr key={(i = i + 1)}>
-                  <td>{e[0].id}</td>
-                  <td>{e[0].name}</td>
-                  <td>{e[0].preparation_time}</td>
-                  <td>
-                    {" "}
-                    <img
-                      style={{ width: "3rem", height: "3rem" }}
-                      alt={e[0].name}
-                      src={e[0].thumbnail}
-                    />
-                  </td>
-                </tr>
-              ))
-            : dispatch.map((e, i) => (
-                <tr key={(i = i + 1)}>
-                  <td>{e.id}</td>
-                  <td>{e.name}</td>
-                  <td>{e.preparation_time}</td>
-                  <td>
-                    {" "}
-                    <img
-                      style={{ width: "3rem", height: "3rem" }}
-                      alt={e.name}
-                      src={e.thumbnail}
-                    />
-                  </td>
-                </tr>
-              ))}
-        </tbody>
-      </ReactBootstrap.Table>
+      <Dispatched orders={dispatch}/>
     </Fragment>
   );
 };
